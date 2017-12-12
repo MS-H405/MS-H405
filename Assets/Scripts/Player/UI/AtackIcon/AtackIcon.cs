c460a01ba9d0d6c0c9e6cc1cf0fe98d85331babc
Syota Obora <bora00940@gmail.com>
2017-12-12 16:20:16 +0900

    【Script】 攻撃アイコンの必殺モード変更時の演出実装

--- Assets/Scripts/Player/UI/AtackIcon/AtackIcon.cs
+++ Assets/Scripts/Player/UI/AtackIcon/AtackIcon.cs
@@ -18,11 +18,19 @@ public class AtackIcon : MonoBehaviour
     private readonly Vector3 MinSize = new Vector3(1.0f, 1.0f, 1.0f);
     private readonly Vector3 MaxSize = new Vector3(2.0f, 2.0f, 2.0f);
 
+    private readonly float SpecialTime = 0.5f;
+    private readonly float SpecialUpHeight = 210.0f;
+    private readonly int SpecialRotAmount = 2;
+
     #endregion
 
     #region variable
 
     [SerializeField] ActionManager.eActionType _actionType;
+
+    // 演出用
+    private Image _image = null;
+    private Sprite _defaultSprite = null;
 
     #endregion
 
@@ -31,12 +39,12 @@ public class AtackIcon : MonoBehaviour
     /// <summary>
     /// 拡縮処理
     /// </summary>
-    public void ScaleChange(bool isAdd, float speed)
+    public void ChangeScale(bool isAdd, float speed)
     {
-        StaticCoroutine.Instance.StartStaticCoroutine(ScaleChangeRun(isAdd, speed));
+        StaticCoroutine.Instance.StartStaticCoroutine(ChangeScaleRun(isAdd, speed));
     }
 
-    private IEnumerator ScaleChangeRun(bool isAdd, float speed)
+    private IEnumerator ChangeScaleRun(bool isAdd, float speed)
     {
         float time = 0.0f;
         Vector3 startSize = transform.localScale;
@@ -50,6 +58,57 @@ public class AtackIcon : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 特殊アイコン変更処理
+    /// </summary>
+    public void ChangeSpecialIcon(bool isStan)
+    {
+        // コルーチン実行
+        StaticCoroutine.Instance.StartStaticCoroutine(ChangeSpecialIconRun(isStan));
+    }
+
+    private IEnumerator ChangeSpecialIconRun(bool isStan)
+    {
+        if(isStan)
+        { 
+            float time = 0.0f;
+            Vector3 min = transform.position;
+            Vector3 max = transform.position + new Vector3(0, SpecialUpHeight, 0);
+
+            // 上にくるくると回転
+            while (time < 1.0f)
+            {
+                transform.position = Vector3.Lerp(min, max, time);
+                transform.eulerAngles += new Vector3(0, (360 * SpecialRotAmount) * (Time.deltaTime / SpecialTime), 0);
+                time += Time.deltaTime / SpecialTime;
+                yield return null;
+            }
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+            // 頂点到達でテクスチャ差し替え
+            _image.sprite = AtackIconManager.Instance.SpecialIconSprite;
+
+            // 下にくるくると回転
+            time = 0.0f;
+            while (time < 1.0f)
+            {
+                transform.position = Vector3.Lerp(max, min, time);
+                transform.eulerAngles += new Vector3(0, (360 * SpecialRotAmount) * (Time.deltaTime / SpecialTime), 0);
+                time += Time.deltaTime / SpecialTime;
+                yield return null;
+            }
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            // コルーチン再開時に差し替えるため
+            yield return new WaitForSeconds(100.0f);
+
+            // 戻すときはムービー中にそのまま差し替え
+            _image.sprite = _defaultSprite;
+        }
+    }
+
     #endregion
 
     #region unity_event
@@ -59,11 +118,15 @@ public class AtackIcon : MonoBehaviour
     /// </summary>
     private void Awake()
     {
+        // SpriteやColorの変更があるため初期化時に取得
+        _image = GetComponent<Image>();
+        _defaultSprite = _image.sprite;
+
         if (StageData.Instance.StageNumber >= (int)_actionType)
             return;
 
         // TODO : 使えないアイコンの処理
-        GetComponent<Image>().color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+        _image.color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
     }
 
     /// <summary>
@@ -71,7 +134,9 @@ public class AtackIcon : MonoBehaviour
     /// </summary>
     private void Update ()
     {
-        transform.eulerAngles = new Vector3(0, 0, 0);
+        Vector3 angle = transform.eulerAngles;
+        angle.z = 0.0f;
+        transform.eulerAngles = angle;
     }
 
     #endregion
