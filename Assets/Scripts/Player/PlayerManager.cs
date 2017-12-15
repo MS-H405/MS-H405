5aa6a3d326c4b37e0d2472b88a47e1a389fb5800
Syota Obora <bora00940@gmail.com>
2017-12-15 20:38:29 +0900

    【Script】 ダメージ時のアニメーションの実装と、外部からの参照用汎用関数をPlayerManagerに追加

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -19,8 +19,19 @@ public class PlayerManager : SingletonMonoBehaviour<PlayerManager>
 
     #region variable
 
-    [SerializeField] Player _player = null;
+    private Player _player = null;
     public Player Player { get { return _player; } private set { _player = value; } }
+    public GameObject PlayerObj { get { return _player.gameObject; } }
+
+    private Transform _playerRightHand = null;
+    public Transform PlayerRightHand { get { return _playerRightHand; } }
+    private Transform _playerLeftHand = null;
+    public Transform PlayerLeftHand { get { return _playerLeftHand; } }
+
+    private Animator _anim = null;
+    public Animator Anim { get { return _anim; } private set { _anim = value; } }
+
+    private ActionManager _actionManager = null;
 
     #endregion
 
@@ -37,6 +48,31 @@ public class PlayerManager : SingletonMonoBehaviour<PlayerManager>
         return pos;
     }
 
+    /// <summary>
+    /// Playerの前方の座標を返す
+    /// </summary>
+    public Vector3 GetPlayerForward()
+    {
+        return _player.transform.position + _player.transform.forward;
+    }
+
+    /// <summary>
+    /// 現在の行動が指定された行動中かを判定
+    /// </summary>
+    public bool CheckActionType(ActionManager.eActionType type)
+    {
+        return _actionManager.NowAction == type;
+    }
+
+    /// <summary>
+    /// 現在ダメージアニメーション中かを返す
+    /// </summary>
+    public bool DamageAnimation()
+    {
+        AnimatorStateInfo animStateInfo = _anim.GetCurrentAnimatorStateInfo(0);
+        return animStateInfo.IsName("Base.Down") || animStateInfo.IsName("Base.Up");
+    }
+
     #endregion
 
     #region unity_event
@@ -46,20 +82,29 @@ public class PlayerManager : SingletonMonoBehaviour<PlayerManager>
     /// </summary>
     private void Awake()
     {
-        if (_player)
-            return;
-
-        // 登録が無ければ探索
-        GameObject obj = GameObject.FindGameObjectWithTag("Player");
-
-        if(!obj)
+        // エラー回避
+        if (!_player)
         {
-            // 警告文 : これが出てたらゲーム成立しない
-            Debug.LogError("Playerが存在しません");
-            return;
+            // 登録が無ければ探索
+            GameObject obj = GameObject.FindGameObjectWithTag("Player");
+
+            if (!obj)
+            {
+                // 警告文 : これが出てたらゲーム成立しない
+                Debug.LogError("Playerが存在しません");
+                return;
+            }
+
+            _player = obj.GetComponent<Player>();
         }
 
-        _player = obj.GetComponent<Player>();
+        // 初期化処理
+        _anim = _player.GetComponent<Animator>();
+        _actionManager = _player.GetComponent<ActionManager>();
+
+        List<GameObject> allChild = GetAllChildren.GetAll(_player.gameObject);
+        _playerLeftHand = allChild.Where(_ => _.name == "USER_20171210_1546:USER_20171210_1546:Character1_LeftForeArm").FirstOrDefault().transform;
+        _playerRightHand = allChild.Where(_ => _.name == "USER_20171210_1546:USER_20171210_1546:Character1_RightForeArm").FirstOrDefault().transform;
     }
 
     #endregion
