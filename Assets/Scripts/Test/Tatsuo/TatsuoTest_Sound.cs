83ac9fecaab9b43bd1435226a9b87a5019f10d05
Syota Obora <bora00940@gmail.com>
2018-01-06 21:13:44 +0900

    消えてしまったリソースと新規metaData

--- Assets/Scripts/Test/Tatsuo/TatsuoTest_Sound.cs
+++ Assets/Scripts/Test/Tatsuo/TatsuoTest_Sound.cs
@@ -8,7 +8,7 @@ public class TatsuoTest_Sound : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		SoundManager.Instance.PlayBGM(SoundManager.eBgmValue.BGM_TITLE);
+		//SoundManager.Instance.PlayBGM(SoundManager.eBgmValue.BGM_TITLE);
 	}
 	
 	// Update is called once per frame
