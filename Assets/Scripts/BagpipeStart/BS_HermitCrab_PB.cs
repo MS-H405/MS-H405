defcb6366721ba7d5d37a5d1f3e00fa433e557ca
Syota Obora <bora00940@gmail.com>
2018-01-09 22:50:44 +0900

    【Script】 ヤドカリ開始時のサウンドを追加

--- Assets/Scripts/BagpipeStart/BS_HermitCrab_PB.cs
+++ Assets/Scripts/BagpipeStart/BS_HermitCrab_PB.cs
@@ -296,6 +296,7 @@ public class BS_HermitCrab_PB : PlayableBehaviour
 			State = STATE_HERMITCRAB.ROAR;
 
 			cs_SetEffekseerObject.NewEffect(0);
+            SoundManager_Tatsuo.Instance.PlaySE(SoundManager_Tatsuo.eSeValue.SP_Blast);
 		}
 	}
 
