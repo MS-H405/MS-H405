5d8d0655f3eb689c655a9e0e674bfe75cc59d0cf
harata <harata.tatsuo@gmail.com>
2018-01-11 01:42:18 +0900

    【Asset】メカ大道芸人登場、SoundManager_Tatsuo.csをMovieSoundManager.csに変更

--- Assets/Plugins/Effekseer/Plugins/EffekseerSystem.cs
+++ Assets/Plugins/Effekseer/Plugins/EffekseerSystem.cs
@@ -28,7 +28,8 @@ public class EffekseerSystem : MonoBehaviour
 	/// <summary xml:lang="ja">
 	/// エフェクトインスタンスの最大数
 	/// </summary>
-	public int effectInstances	= 1600;
+	// 最初 : 1600
+	public int effectInstances = 5000;		// メカ大道芸人登場で1600では足りなくなったので、とりあえず5000に
 
 	/// <summary xml:lang="en">
 	/// Maximum number of quads that can be drawn.
@@ -36,7 +37,8 @@ public class EffekseerSystem : MonoBehaviour
 	/// <summary xml:lang="ja">
 	/// 描画できる四角形の最大数
 	/// </summary>
-	public int maxSquares		= 8192;
+	public int maxSquares = 8192;
+	//public int maxSquares = 1000;
 
 	/// <summary xml:lang="en">
 	/// The coordinate system of effects.
