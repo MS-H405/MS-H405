d5ec74e71f2dc06463f6a5efb6b7edc40946cefb
Syota Obora <bora00940@gmail.com>
2018-01-16 20:42:18 +0900

    【Script】 ナイフ投げ攻撃を実装

---
+++ Assets/Scripts/Enemy/MechaPiero/Knife.cs
@@ -0,0 +1,101 @@
+﻿// ---------------------------------------------------------
+// Knife.cs
+// 概要 : 
+// 作成者: Shota_Obora
+// ---------------------------------------------------------
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+  
+public class Knife : MonoBehaviour
+{
+    #region define
+
+    #endregion
+
+    #region variable
+
+    #endregion
+
+    #region method
+
+    /// <summary>
+    /// 実行処理
+    /// </summary>
+    public IEnumerator Run()
+    {
+        // 敵の方を向く
+        // TODO : とりあえず一瞬で向く
+        transform.LookAt(PlayerManager.Instance.Player.transform.position + new Vector3(0, 1.0f, 0));
+        transform.localEulerAngles += new Vector3(0,0,90);
+
+        float time = 0.0f;
+        while(time < 0.5f)
+        {
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                transform.position += transform.forward * 20.0f * Time.deltaTime;
+            });
+    }
+
+    /// <summary>
+    /// 出現処理
+    /// </summary>
+    public IEnumerator Init()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        GameEffectManager.Instance.Play("KnifeStart", transform.position + (transform.forward * 2.0f));
+
+        float time = 0.0f;
+        while(time < 0.25f)
+        {
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    #endregion
+
+    #region unity_event
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    private void Start()
+    {
+        StaticCoroutine.Instance.StartStaticCoroutine(Init());
+    }
+
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            if (!col.GetComponent<Player>().Damage())
+                return;
+
+            GameEffectManager.Instance.Play("PinAttack", transform.position);
+            Destroy(gameObject);
+        }
+        if (col.tag == "Field")
+        {
+            GameEffectManager.Instance.Play("KnifeLose", transform.position + transform.forward);
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}  
---
+++ Assets/Scripts/Enemy/MechaPiero/KnifeManager.cs
@@ -0,0 +1,82 @@
+﻿// ---------------------------------------------------------
+// KnifeManager.cs
+// 概要 : 
+// 作成者: Shota_Obora
+// ---------------------------------------------------------
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+  
+public class KnifeManager : MonoBehaviour
+{
+    #region define
+
+    #endregion
+
+    #region variable
+
+    List<Knife> _knifeList = new List<Knife>();
+
+    #endregion
+
+    #region method
+
+    /// <summary>
+    /// 実行処理
+    /// </summary>
+    private IEnumerator Run()
+    {
+        float time = 0.0f;
+        while(time < 1.0f)
+        {
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        int index = 0;
+        while (index < _knifeList.Count)
+        {
+            time = 0.0f;
+            while (time < 0.4f)
+            {
+                time += Time.deltaTime;
+                yield return null;
+            }
+            StaticCoroutine.Instance.StartStaticCoroutine(_knifeList[index].Run());
+            index++;
+        }
+    }
+
+    #endregion
+
+    #region unity_event
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    private void Awake()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _knifeList.Add(transform.GetChild(i).GetComponent<Knife>());
+        }
+        StaticCoroutine.Instance.StartStaticCoroutine(Run());
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    private void Update()
+    {
+        if (transform.childCount > 0)
+            return;
+
+        Destroy(gameObject);
+    }
+
+    #endregion
+}  
