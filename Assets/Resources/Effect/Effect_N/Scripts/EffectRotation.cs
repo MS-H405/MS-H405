eb1d3c47d012ee72df1a86233642b2be8da30252
Syota Obora <bora00940@gmail.com>
2018-01-17 18:03:51 +0900

    【Asset】 Effectのmaterialの新規追加

---
+++ Assets/Resources/Effect/Effect_N/Scripts/EffectRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectRotation : MonoBehaviour 
+{
+    [SerializeField]
+    private float fRotationSpeedX;       // X回転スピード
+    [SerializeField]
+    private float fRotationSpeedY;       // Y回転スピード
+    [SerializeField]
+    private float fRotationSpeedZ;       // Z回転スピード
+
+    // ===== 更新 =====
+    void Update()
+    {
+        // オブジェクトを回転させる
+        this.transform.Rotate(fRotationSpeedX, fRotationSpeedY, fRotationSpeedZ);
+    }
+}
