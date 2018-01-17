21863e6435429dd533ea270915fbb091ec6a93ce
Syota Obora <bora00940@gmail.com>
2018-01-18 00:05:15 +0900

    【Script】 Titleのエフェクトを変更

---
+++ Assets/Scripts/Utility/PlayOnEmit.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------
+// PlayOnEmit.cs
+// 概要 : 
+// 作成者: Shota_Obora
+// ---------------------------------------------------------
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+  
+public class PlayOnEmit : MonoBehaviour
+{
+    #region variable
+
+    [SerializeField] int _emitAmount = 100;
+
+    #endregion
+
+    #region unity_event
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    private void Awake()
+    {
+        GetComponent<ParticleSystem>().Emit(_emitAmount);
+    }
+
+    #endregion
+}  
