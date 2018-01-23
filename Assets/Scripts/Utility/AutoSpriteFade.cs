0803fa2c49f45db73f62e32b3b1d684d0527e586
Syota Obora <bora00940@gmail.com>
2018-01-23 21:04:35 +0900

    【Script】 技習得時のUI演出実装

---
+++ Assets/Scripts/Utility/AutoSpriteFade.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------
+// AutoSpriteFade.cs
+// 概要 : 
+// 作成者: Shota_Obora
+// ---------------------------------------------------------
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+  
+public class AutoSpriteFade : MonoBehaviour
+{
+    #region define
+
+    #endregion
+
+    #region variable
+
+    [SerializeField] float _speed = 0.5f;
+
+    #endregion
+
+    #region method
+
+    #endregion
+
+    #region unity_event
+    
+    /// <summary>
+    /// 更新前処理
+    /// </summary>
+    private void Start ()
+    {
+        Image image = GetComponent<Image>();
+        float time = 0.0f;
+        var fadeDisposable = new SingleAssignmentDisposable();
+        fadeDisposable.Disposable = this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                time += Time.deltaTime / _speed;
+                image.color = Color.Lerp(new Color(1, 1, 1, 0), Color.white, time);
+
+                if(time >= 1.0f)
+                {
+                    image.color = Color.white;
+                    fadeDisposable.Dispose();
+                    enabled = false;
+                }
+            });
+    }
+
+    #endregion
+}  
