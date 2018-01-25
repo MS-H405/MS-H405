05e5ec97afd61a38a2fa5fd567dee23aca4f1c13
Syota Obora <bora00940@gmail.com>
2018-01-25 15:41:01 +0900

    【Script】 Debug用タイムスケール変動処理実装

---
+++ Assets/Scripts/Utility/DebugTimeChanger.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------
+// DebugTimeChanger.cs
+// 概要 : 
+// 作成者: Shota_Obora
+// ---------------------------------------------------------
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UniRx.Triggers;
+  
+public class DebugTimeChanger : MonoBehaviour
+{
+    #region unity_event
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Time.timeScale = 1.0f;
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Time.timeScale = 1.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Time.timeScale = 2.0f;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    #endregion
+}  
